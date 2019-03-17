53929d8e0101778fe579c3f7d57bdaed9981a036
Вова <kroxx.gm@gmail.com>
2019-03-17 22:31:32 +0200

    Fix django.jQuery select change events

--- jet/static/jet/js/src/features/selects.js
+++ jet/static/jet/js/src/features/selects.js
@@ -188,11 +188,7 @@ Select2.prototype = {
                 }
             };
         }
-
-        $select.on('change', function(e) {
-            django.jQuery($select.get(0)).trigger(e);
-        });
-
+        
         $select.select2(settings);
     },
     initSelect2: function() {
